ad677fbd97be9a61a766f0393150575d33bdbcf8
rmrah <rmrah@DELL>
2024-06-20 19:49:25 +0530

    Add project files.

---
+++ TaskManager/TaskManager/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
